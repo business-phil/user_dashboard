9bafb9423e81369713ec94898732b61090bddefa
Phil Boothe <pfb3cn@virginia.edu>
2016-08-23 08:23:30 -0400

    initial commit

---
+++ user_dashboard/urls.py
@@ -0,0 +1,23 @@
+"""user_dashboard URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('apps.login.urls')),
+    url(r'^users/', include('apps.dashboard.urls')),
+]
