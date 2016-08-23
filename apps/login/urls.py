95e3c15312dc0812c109f228536ef1cab8218aa9
Phil Boothe <pfb3cn@virginia.edu>
2016-08-23 14:47:16 -0400

    added User model

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login$', views.index, name="login"),  #POST
-    url(r'^register$', views.register, name="register"),  #POST
+    url(r'^login$', views.index, name='login'),  #POST
+    url(r'^register$', views.register, name='register'),  #POST
 ]
