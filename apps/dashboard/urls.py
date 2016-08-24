00a5b98e86290e04d526594a8837146563349ea7
jacobsaw <adam.w.jacobs@gmail.com>
2016-08-24 10:43:49 -0400

    made corrections to models, views and show.html pages so that comments/messages function properly.  Added a walluser foreign key field to messages so that messages on wall are specific to each user.

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     url(r'^new$', views.new_admin, name='new_admin'),
     url(r'^create$', views.create_admin, name='create_admin'),  #POST
     url(r'^show/(?P<user_id>\d+)$', views.show, name='show'),
-    url(r'^show/new$', views.new_message, name="new_message"),  #POST
-    url(r'^show/(?P<message_id>\d+)/new$', views.new_comment, name="new_comment"),  #POST
+    url(r'^show/(?P<user_id>\d+)/new$', views.new_message, name="new_message"),  #POST
+    url(r'^show/(?P<user_id>\d+)/(?P<message_id>\d+)/new$', views.new_comment, name="new_comment"),  #POST
 ]
 
 '''
