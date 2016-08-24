0052c38f82a5f9cfbf407b2656db42976b0535c9
Phil Boothe <pfb3cn@virginia.edu>
2016-08-23 20:08:38 -0400

    login and registration are fully functional! (addtl testing needed)

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login$', views.index, name='login'),  #POST
-    url(r'^register$', views.register, name='register'),  #POST
+    url(r'^login$', views.login, name='login'),
+    url(r'^register$', views.register, name='register'),
 ]
--- apps/login/views.py
+++ apps/login/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
 from .models import User
 from .forms import RegisterForm, LoginForm
 
@@ -20,14 +22,17 @@ def register(request):
     else:
         for message in regstatus[1]:
             messages.warning(request, message)
-        return redirect(reverse('loginreg:index'))
+        return redirect(reverse('login:index'))
 
 # Submit login form
 def login(request):
-    loginstatus = User.usermanager.login(request.POST['email'], request.POST['password'])
+    loginstatus = User.userManager.login(request.POST['email'], request.POST['password'])
+    print "MAYBE HERE?!"
     if loginstatus[0]:
         request.session['user_id'] = loginstatus[1]
-        return redirect(reverse('books:index'))
+        print "THSI SHOULD WORK"
+        return redirect(reverse('dashboard:index'))
     else:
         messages.warning(request, loginstatus[1])
-        return redirect(reverse('loginreg:index'))
+        print "DIDNT WORK SRY"
+        return redirect(reverse('login:index'))
