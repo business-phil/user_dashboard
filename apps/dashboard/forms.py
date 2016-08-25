4383122fcb2975c6effce5bbea0f7fb082679c50
Phil Boothe <pfb3cn@virginia.edu>
2016-08-25 17:40:59 -0400

    made edit routes more efficient; adjusted navbar bootstrap

--- apps/dashboard/forms.py
+++ apps/dashboard/forms.py
@@ -7,16 +7,13 @@ class EditForm(forms.Form):
 	first_name = forms.CharField(max_length=45)
 	last_name = forms.CharField(max_length=45)
 
-	def __init__(self, *args, **kwargs):
+	def __init__(self, **kwargs):
 		user = kwargs.pop('user', None)
-		super(EditForm, self).__init__( *args, **kwargs)
+		super(EditForm, self).__init__()
 		if user:
 			self.fields['email'].initial = user.email
 			self.fields['first_name'].initial = user.first_name
 			self.fields['last_name'].initial = user.last_name
-
-
-
 
 class EditAdminForm(forms.Form):
 	email = forms.EmailField()
@@ -24,26 +21,24 @@ class EditAdminForm(forms.Form):
 	last_name = forms.CharField(max_length=45)
 	user_level = forms.ChoiceField([('admin', 'admin'), ('user', 'user')])
 
-	def __init__(self, *args, **kwargs):
+	def __init__(self, **kwargs):
 		user = kwargs.pop('user', None)
-		super(EditAdminForm, self).__init__( *args, **kwargs)
+		super(EditAdminForm, self).__init__()
 		if user:
 			self.fields['email'].initial = user.email
 			self.fields['first_name'].initial = user.first_name
 			self.fields['last_name'].initial = user.last_name
 			self.fields['user_level'].initial = user.user_level
 
-
 class EditPasswordForm(forms.Form):
 	password = forms.CharField(max_length=45, widget=forms.PasswordInput)
 	confirm_password = forms.CharField(max_length=45, widget=forms.PasswordInput)
 
-
 class EditDescriptionForm(forms.Form):
 	description = forms.CharField(max_length=255, widget=forms.Textarea, label='')
 
-	def __init__(self, *args, **kwargs):
+	def __init__(self, **kwargs):
 		user = kwargs.pop('user', None)
-		super(EditDescriptionForm, self).__init__( *args, **kwargs)
+		super(EditDescriptionForm, self).__init__()
 		if user:
 			self.fields['description'].initial = user.description
