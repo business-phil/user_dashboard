f2967206ea0d92696f5f0b0f4a0e693f44f03630
Phil Boothe <pfb3cn@virginia.edu>
2016-08-25 15:13:17 -0400

    organized views and models

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -6,83 +6,64 @@ from .forms import RegisterForm, EditForm, EditAdminForm, EditPasswordForm, Edit
 
 # user/admin home page
 def index(request):
+    if not 'user_id' in request.session:
+        return redirect(reverse('login:index'))
     current_user = User.objects.get(id=request.session['user_id'])
     user = User.objects.all()
     context = { 'users': user }
-    if current_user.user_level == 'user':
-        return render(request, 'dashboard/index.html', context)
-    else: #only two choices for user level - admin and user
+    if current_user.user_level == 'admin':
         return render(request, 'dashboard/index_admin.html', context)
+    else:
+        return render(request, 'dashboard/index.html', context)
 
 # users page to edit own info
 def edit(request, user_id):
-    #get the user object for whomever is in session
+    if not 'user_id' in request.session:
+        return redirect(reverse('login:index'))
     user = User.objects.get(id=request.session['user_id'])
-    print user_id, type(user_id)
-    print user.id, type(user.id)
-    #if the user_level is 'user', create the appropiate forms and strings
-    if user.id == int(user_id) and user.user_level == 'user':
-        editForm = EditForm(user=user)
-        editDescriptionForm = EditDescriptionForm(user=user)
-        editStr = 'profile'
-        title = 'Profile'
-        hidden = ''
-    elif user.id != int(user_id) and user.user_level == 'user':
+    # redirect user accessing other user pages
+    if user.id != int(user_id) and user.user_level == 'user':
         return redirect(reverse('dashboard:edit', args=[user.id]))
-    #if the user_level is 'admin' and he is trying to edit himself
-    #then create the appropiate forms and strings
-    elif user.user_level == 'admin' and user.id == int(user_id):
-        editForm = EditAdminForm(user=user)
-        editDescriptionForm = EditDescriptionForm(user=user)
-        editStr = 'profile'
-        title = 'Profile'
-        hidden = ''
-    #if the user_level is admin and he is trying to edit someone else
-    #then create the appropiate forms and strings
-    else:
-        user = User.objects.get(id=user_id)
-        editForm = EditAdminForm(user=user)
-        editDescriptionForm = ''
-        editStr = "user #" + str(user_id)
-        title = 'User'
-        hidden = 'hidden'
-    #create editPasswordForm
-    editPasswordForm = EditPasswordForm()
+    urlUser = User.objects.get(id=user_id)
+    editForm = EditForm(user=user) if user.user_level == 'user' else EditAdminForm(user=urlUser)
+    editDescription = EditDescriptionForm(user=user) if user.id == int(user_id) else ''
     context = {
+        'user': urlUser,
         'editForm': editForm,
-        'editPasswordForm': editPasswordForm,
-        'editDescriptionForm': editDescriptionForm,
-        'editStr': editStr,
-        'title':title,
-        'user_id': user_id,
-        'hidden': hidden,
+        'editDescriptionForm': editDescription,
+        'editPasswordForm': EditPasswordForm(),
     }
     return render(request, 'dashboard/edit.html', context)
 
 # POST request for user editing info
-def edit_info(request, user_id):  #POST REQUEST
+def edit_info(request, user_id):
     editStatus = User.userManager.editInfo(user_id, **request.POST)
     return redirect(reverse('dashboard:index'))
 
 # POST request for user editing password
-def edit_pw(request, user_id):  #POST REQUEST
+def edit_pw(request, user_id):
     editStatus = User.userManager.editPassword(user_id, **request.POST)
     return redirect(reverse('dashboard:index'))
 
 # POST request for user editing description
-def edit_desc(request, user_id):  #POST REQUEST
+def edit_desc(request, user_id):
     editStatus = User.userManager.editDescription(user_id, request.POST['description'])
     return redirect(reverse('dashboard:index'))
 
 # POST link for delete user link (admin only)
-def remove_admin(request, user_id):  #POST REQUEST
-    User.objects.get(id= user_id).delete()
+def remove_admin(request, user_id):
+    User.objects.get(id=user_id).delete()
     return redirect(reverse('dashboard:index'))
 
 # admin page to create new user
 def new_admin(request):
-    context = {'regform': RegisterForm }
-    return render(request, 'dashboard/new_admin.html', context)
+    if not 'user_id' in request.session:
+        return redirect(reverse('login:index'))
+    if User.objects.get(id=request.session['user_id']).user_level == 'admin':
+        context = {'regform': RegisterForm() }
+        return render(request, 'dashboard/new_admin.html', context)
+    else:
+        return redirect(reverse('dashboard:index'))
 
 # POST link for new user form (admin only)
 def create_admin(request):
@@ -96,7 +77,9 @@ def create_admin(request):
 
 # show user wall
 def show(request, user_id):
-    messages = Message.objects.filter(walluser=user_id).order_by('-created_at')
+    if not 'user_id' in request.session:
+        return redirect(reverse('login:index'))
+    messages = Message.objects.filter(walluser=user_id).order_by('-id')
     comments = Comment.objects.all()
     user = User.objects.get(id=user_id)
     context = {
@@ -106,7 +89,7 @@ def show(request, user_id):
     }
     return render(request, 'dashboard/show.html', context)
 # POST request for adding message
-def new_message(request, user_id):  #POST REQUEST
+def new_message(request, user_id):
     messageuser = User.objects.get(id=request.session['user_id'])
     walluser = User.objects.get(id=user_id)
     message = request.POST['message']
@@ -114,22 +97,9 @@ def new_message(request, user_id):  #POST REQUEST
     return redirect(reverse('dashboard:show', args=user_id))
 
 # POST request for adding comment
-def new_comment(request, user_id, message_id):  #POST REQUEST
+def new_comment(request, user_id, message_id):
     user = User.objects.get(id=request.session['user_id'])
     message = Message.objects.get(id=message_id)
     comment = request.POST['comment']
     Comment.objects.create(user=user, message=message, comment=comment)
     return redirect(reverse('dashboard:show', args=user_id))
-
-'''
-TO BE ADDED:
-- class-based views
-    ex: class New(View):
-            def get(self, request):
-                # renders new_admin.html
-            def post(self, request):
-                # processes POST request from FORM on new_admin.html
-
-- user link to delete own message
-- user link to delete own comment
-'''
--- apps/login/models.py
+++ apps/login/models.py
@@ -4,13 +4,14 @@ from re import match, search
 from bcrypt import hashpw, gensalt
 
 class UserManager(models.Manager):
+    # REGISTER form - login/index
     def register(self, email, first_name, last_name, password, confirm_password, csrfmiddlewaretoken):
         messagelist = []
         # Validate email
         email = email[0]
         if not match(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$', email):
             messagelist.append("Invalid email address")
-        elif User.objects.filter(email=email):
+        elif self.filter(email=email):
             messagelist.append("Email Address already in use")
         # Validate first_name
         first_name = first_name[0]
@@ -33,29 +34,28 @@ class UserManager(models.Manager):
         if conf_password != password:
             messagelist.append("Password does not match")
         # Check if all validation checks passed
-        if len(messagelist) == 0:
-            pw_hash = hashpw(password.encode(), gensalt())
-            # Make first registered user an admin
-            if User.objects.all():
-                new_user = User.objects.create(email=email, first_name=first_name, last_name=last_name, password=pw_hash)
-            else:
-                new_user = User.objects.create(email=email, first_name=first_name, last_name=last_name, password=pw_hash, user_level = 'admin')
-            return (True, new_user.id)
-        else:
+        if len(messagelist) > 0:
             return (False, messagelist)
+        pw_hash = hashpw(password.encode(), gensalt())
+        new_user = self.create(email=email, first_name=first_name, last_name=last_name, password=pw_hash)
+        # Make first registered user an admin
+        if new_user.id == 1:
+            new_user.update(user_level='admin')
+        return (True, new_user.id)
+    # LOGIN form - login/index
     def login(self, email, password):
-        if User.objects.filter(email=email):
-            user = User.objects.get(email=email)
+        if self.filter(email=email):
+            user = self.get(email=email)
             if hashpw(password.encode(), user.password.encode()) == user.password:
                 return (True, user.id)
             else:
                 return (False, "Invalid password")
         else:
             return (False, "Invalid email address")
-
+    # EDIT INFO form - dashboard/edit/user_id
     def editInfo(self, user_id, email, first_name, last_name, csrfmiddlewaretoken, user_level=['user']):
         messageList = []
-        #Validate email
+        # Validate email
         email = email[0]
         if not match(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$', email):
             messagelist.append("Invalid email address")
@@ -71,16 +71,16 @@ class UserManager(models.Manager):
             messagelist.append("Last Name must be at least 2 characters long")
         elif search(r'[^a-zA-Z]', last_name):
             messagelist.append("Last Name must only contain letters")
-        
+        # Validate user_level
         user_level = user_level[0]
-        #validations
+        if user_level != 'user' and user_level != 'admin':
+            messagelist.append("User Level must be 'user' or 'admin'")
+        # Check if all validations passed
         if len(messageList) > 0:
             return (False, messageList)
-
         self.filter(id=user_id).update(email=email, first_name=first_name, last_name=last_name, user_level=user_level)
-
         return (True, ["User Info updated successfully"])
-
+    # CHANGE PASSWORD form - dashboard/edit/user_id
     def editPassword(self, user_id, password, confirm_password, csrfmiddlewaretoken):
         messagelist = []
         # Validate password
@@ -94,15 +94,14 @@ class UserManager(models.Manager):
         # Check if all validation checks passed
         if len(messagelist) > 0:
             return(False, messagelist)
-
         pw_hash = hashpw(password.encode(), gensalt())
-
         self.filter(id=user_id).update(password=pw_hash)
-
         return (True, ["User Info updated successfully"])
-
+    # EDIT DESCRIPTION form - dashboard/edit/user_id
     def editDescription(self, user_id, description):
-        #no validations
+        # Validate description
+        if len(description) == 0:
+            description = ""
         self.filter(id=user_id).update(description=str(description))
         return (True, ["Description Successfully Updated"])
 
